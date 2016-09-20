da480fde8dc0d27d80bb38440b3d320f748ad6aa
ncaq <ncaq@ncaq.net>
2016-09-20 13:40:44 +0900

    changed: 表示箇所をrightからleftに変更

--- mikutter_header_client.rb
+++ mikutter_header_client.rb
@@ -1,8 +1,8 @@
 Plugin.create(:mikutter_header_client) {
   class Gdk::MiraclePainter
-    alias :orig_header_right :header_right
-    def header_right(context = dummy_context)
-      layout = orig_header_right(context)
+    alias :orig_header_left :header_left
+    def header_left(context = dummy_context)
+      layout = orig_header_left(context)
       layout.text += " via " + message[:source]
       layout
     end
